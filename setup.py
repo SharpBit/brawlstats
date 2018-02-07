c82fae2d43622febd8be0d3c84e0fc3fbb1830d1
SharpBit <31069084+SharpBit@users.noreply.github.com>
2018-02-07 13:00:25 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name='abrawl-py',
+    version='1.0.0',
+    description='An async Python API wrapper for the Brawl Stars API',
+    long_description=long_description,
+    url='https://github.com/SharpBit/brawlstars',
+    author='SharpBit',
+    author_email='uworst888@gmail.com',
+    license='MIT',
+    keywords=['brawl stars, abrawl-py, api-wrapper, async'],
+    packages=find_packages(),
+    install_requires=['aiohttp', 'python-box']
+)
