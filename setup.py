68d0219343eefc7e5eee0f44ae2bc8c76835aef8
SharpBit <31069084+SharpBit@users.noreply.github.com>
2018-11-20 21:16:26 -0500

    actually this time and new version for docs

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('README.rst', encoding='utf8') as f:
+    long_description = f.read()
+
 setup(
     name='brawlstats',
-    version='2.0.0',
+    version='2.0.2',
     description='An async Python API wrapper for the unofficial Brawl Stars API',
-    long_description='This package include an easy to use async Client to get Brawl Stars player and band statistics, and the leaderboard. Github: https://github.com/SharpBit/brawlstats',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     url='https://github.com/SharpBit/brawlstats',
     author='SharpBit',
     author_email='sharpbit3618@gmail.com',
