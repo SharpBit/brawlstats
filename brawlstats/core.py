64c8e0d5ac5b9f0c1b1e7d7246e675f64decaa23
SharpBit <31069084+SharpBit@users.noreply.github.com>
2018-12-05 17:54:54 -0500

    remove connector

--- brawlstats/core.py
+++ brawlstats/core.py
@@ -65,7 +65,7 @@ class Client:
 
     def __init__(self, token, **options):
         self.is_async = options.get('is_async', False)
-        self.session = options.get('session') or (aiohttp.ClientSession(connector=connector) if self.is_async else requests.Session())
+        self.session = options.get('session') or (aiohttp.ClientSession() if self.is_async else requests.Session())
         self.timeout = options.get('timeout', 10)
         self.api = API(options.get('url'))
         self.headers = {
