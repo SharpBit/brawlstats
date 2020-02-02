2a84a7245f357017e712e9453776f9f90c89c040
SharpBit <31069084+SharpBit@users.noreply.github.com>
2020-02-02 14:08:03 -0500

    error changes

--- brawlstats/errors.py
+++ brawlstats/errors.py
@@ -1,69 +1,74 @@
 class RequestError(Exception):
     """The base class for all errors."""
 
-    def __init__(self, code, error, text=None, retry_after=None):
+    def __init__(self, code, message, retry_after=None):
         pass
 
 
 class Unauthorized(RequestError):
     """Raised if your API Key is invalid or blocked."""
 
-    def __init__(self, url, code):
+    def __init__(self, code, url):
         self.code = code
-        self.error = 'Your API Key is invalid or blocked.\nURL: ' + url
-        super().__init__(self.code, self.error)
+        self.url = url
+        self.message = 'Your API Key is invalid or blocked.'
+        super().__init__(self.code, self.message)
 
 
 class Forbidden(RequestError):
     """Raised if the IP using the token was not whitelisted."""
 
-    def __init__(self, url, code, message):
+    def __init__(self, code, url, message):
         self.code = code
-        self.error = '{}\nURL: {}'.format(message, url)
-        super().__init__(self.code, self.error)
+        self.url = url
+        self.message = message
+        super().__init__(self.code, self.message)
 
 
 class NotFoundError(RequestError):
     """Raised if an invalid player tag or club tag has been passed."""
 
-    def __init__(self, invalid_chars, code):
+    def __init__(self, code, invalid_chars=[]):
         self.code = code
-        self.error = 'An incorrect tag has been passed.\nInvalid Characters: ' + ', '.join(invalid_chars)
-        super().__init__(self.code, self.error)
+        self.message = 'An incorrect tag has been passed.\nInvalid Characters: ' + ', '.join(invalid_chars)
+        super().__init__(self.code, self.message)
 
 
 class RateLimitError(RequestError):
     """Raised when the rate limit is reached."""
-    def __init__(self, url, code, retry_after):
+    def __init__(self, code, url, retry_after):
         self.code = code
+        self.url = url
         self.retry_after = retry_after
-        self.error = 'The rate limit has been reached.\nURL:{}\nRetry after: {}s'.format(url, retry_after)
-        super().__init__(self.code, self.error, retry_after=self.retry_after)
+        self.message = 'The rate limit has been reached.\nRetry after: {}s'.format(retry_after)
+        super().__init__(self.code, self.message, retry_after=self.retry_after)
 
 
 class UnexpectedError(RequestError):
     """Raised if an unknown error has occured."""
 
-    def __init__(self, url, code, data):
+    def __init__(self, url, code, text):
         self.code = code
-        self.error = 'An unexpected error has occured.\nURL: ' + url
-        self.text = data
-        super().__init__(self.code, self.error, text=self.text)
+        self.url = url
+        self.message = 'An unexpected error has occured.\n{text}'
+        super().__init__(self.code, self.message)
 
 
 class ServerError(RequestError):
     """Raised if the API is down."""
 
-    def __init__(self, url, code):
+    def __init__(self, code, url):
         self.code = code
-        self.error = 'The API is down. Please be patient and try again later.\nURL: ' + url
-        super().__init__(self.code, self.error)
+        self.url = url
+        self.message = 'The API is down. Please be patient and try again later.'
+        super().__init__(self.code, self.message)
 
 
 class MaintenanceError(RequestError):
     """Raised if there is a maintenance break."""
 
-    def __init__(self, url, code):
+    def __init__(self, code, url):
         self.code = code
-        self.error = 'There is currently a maintenance break. Please be patient and try again later.\nURL: ' + url
-        super().__init__(self.code, self.error)
+        self.url = url
+        self.message = 'There is currently a maintenance break. Please be patient and try again later.'
+        super().__init__(self.code, self.message)
