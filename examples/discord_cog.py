604f9fbf96e7184c754472b5697a43ef3ebdf457
SharpBit <31069084+SharpBit@users.noreply.github.com>
2020-03-08 16:29:44 -0400

    change error attribute to message in discord cog example

--- examples/discord_cog.py
+++ examples/discord_cog.py
@@ -15,7 +15,7 @@ class BrawlStars(commands.Cog, name='Brawl Stars'):
         try:
             player = await self.client.get_profile(tag)
         except brawlstats.RequestError as e:  # catches all exceptions
-            return await ctx.send('```\n{}: {}\n```'.format(e.code, e.error))  # sends code and error message
+            return await ctx.send('```\n{}: {}\n```'.format(e.code, e.message))  # sends code and error message
         em = discord.Embed(title='{0.name} ({0.tag})'.format(player))
         em.description = 'Trophies: {}'.format(player.trophies)  # you could make this better by using embed fields
         await ctx.send(embed=em)
