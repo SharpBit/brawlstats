7dcc3e5abb258d5821b390d8772d97e23fed9be5
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2020-06-27 10:02:42 +0300

    little change

--- brawlstats/utils.py
+++ brawlstats/utils.py
@@ -12,7 +12,7 @@ from .errors import NotFoundError
 class API:
     def __init__(self, base_url, version=1):
         self.BASE = base_url or 'https://api.brawlstars.com/v'\
-            '{}'.format(version)
+            + str(version)
         self.PROFILE = self.BASE + '/players'
         self.CLUB = self.BASE + '/clubs'
         self.RANKINGS = self.BASE + '/rankings'
